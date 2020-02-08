fa91f80183021e95a151be4e473fd88e07d6c394
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-09 02:59:59 +0900

    Initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+  def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
+      # like拡張機能
+      @likes = Like.where(micropost_id: params[:micropost_id])
+    end
+  end
+
+  def help
+  end
+
+
+  def about
+  end
+
+  def contact
+  end
+end
