759996a8bef2204d85a6fd8ca0f83c914c8ce851
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-11 21:34:46 +0900

    edit font

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_054225) do
+ActiveRecord::Schema.define(version: 2020_02_11_075215) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_054225) do
     t.string "picture"
     t.integer "likes_count"
     t.integer "time"
+    t.integer "like_count"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
