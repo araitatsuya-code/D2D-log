55db893223d556eef4f2b862366362f9e0197472
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-12 22:22:45 +0900

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
-      flash[:info] = "Please check your email to activate your account."
+      # UserMailer.account_activation(@user).deliver_now
+      # flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 13.115.204.157
+  host = "13.115.204.157"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
