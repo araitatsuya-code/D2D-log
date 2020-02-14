c7d55ad1a5c9bff6a95dd9736d53869807f8b264
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-14 23:04:40 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
-User.create!(name:  "Example User",
-  email: "example@railstutorial.org",
-  password:              "foobar",
-  password_confirmation: "foobar",
+User.create!(name:  "テストさん",
+  email: "test@test.com",
+  password:              "1111",
+  password_confirmation: "1111",
   admin: true,
   activated: true,
   activated_at: Time.zone.now)
 
 
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-    email: email,
-    password:              password,
-    password_confirmation: password,
-    activated: true,
-    activated_at: Time.zone.now)
-end
+# 99.times do |n|
+# name  = Faker::Name.name
+# email = "example-#{n+1}@railstutorial.org"
+# password = "password"
+# User.create!(name:  name,
+#     email: email,
+#     password:              password,
+#     password_confirmation: password,
+#     activated: true,
+#     activated_at: Time.zone.now)
+# end
 
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.microposts.create!(content: content) }
+# end
 
-# リレーションシップ
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+# # リレーションシップ
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
