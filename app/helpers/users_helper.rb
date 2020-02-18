c8b67805113a14e3def3fd9f51b836a936d318d2
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-18 20:37:04 +0900

    Update users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,5 @@
 module UsersHelper
 
-  # 渡されたユーザーのGravatar画像を返す
-  def gravatar_for(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
-
     def sum_times
       @chart.values.inject(:+) unless @chart.nil?
     end
