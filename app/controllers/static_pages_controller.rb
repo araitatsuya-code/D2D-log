3c72071dc8badde7d7411179c39d1af68bdc476e
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-22 20:55:31 +0900

    can_refine

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,6 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10).search(params[:search])
       @likes = Like.where(micropost_id: params[:micropost_id])
-      if params[:tag_name]
-        @microposts = Micropost.tagged_with("#{params[:tag_name]}")
-      end
     end
   end
 
