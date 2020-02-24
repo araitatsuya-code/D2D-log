c461c1e80b82efe90b36b7a2ef386ddc08696acf
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-24 13:23:34 +0900

    gimei

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,31 @@
-User.create!(name:  "テストさん",
-  email: "test@test.com",
-  password:              "111111",
-  password_confirmation: "111111",
-  admin: false,
-  activated: true,
-  activated_at: Time.zone.now)
+# User.create!(name:  "テストさん",
+#   email: "test@test.com",
+#   password:              "111111",
+#   password_confirmation: "111111",
+#   admin: false,
+#   activated: true,
+#   activated_at: Time.zone.now)
 
+
+50.times do |n|
+  gimei = Gimei.new
+  email = "test-#{n+1}@d2d.com"
+  name  = gimei.name.kanji
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    admin: false,
+    activated: true,
+    activated_at: Time.zone.now
+  )
+end
 
 # 99.times do |n|
 # name  = Faker::Name.name
-# email = "example-#{n+1}@railstutorial.org"
+# email = "example-#{n+1}@d2d.org"
 # password = "password"
 # User.create!(name:  name,
 #     email: email,
